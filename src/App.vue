2e8204eaefec6880a6b1a9c7fcd68cc4765e7fc4
Артем Антонянц <aa.artem.0303@mail.ru>
2025-08-14 06:13:50 +0500

    feat(UI): set Roboto font

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,11 @@ import AppTemplate from "@/shared/components/AppTemplate.vue";
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap");
+
 * {
   box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
 }
 body {
   margin: 0;
